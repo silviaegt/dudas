e519f028ef581914ea0dd81afa1a2089f841f79c
Silvia Eunice Gutiérrez de la Torre <segutierrez@colmex.mx>
2021-04-06 22:16:47 -0500

    :wrench: algunos intentos extra

--- regex/procesar_metadata.R
+++ regex/procesar_metadata.R
@@ -6,4 +6,16 @@ lines <- readLines(text)
 View(lines)
 
 authors <- str_subset(lines, "(^Author:.\\w+\\,\\s\\w+)")
-View(authors)
+
+#intento con lag:
+#titles <- lag(str_subset(lines, "(^Author:.\\w+\\,\\s\\w+)"))
+#View(titles)
+#Nota: sólo regresa un string vacío antes del primer patrón detectado
+#Ver: https://twitter.com/espejolento/status/1379633590481260548
+
+
+#intento para extraer títulos:
+#titles <- str_subset(lines, "(\\n).*(\\nAuthor:\\s)")
+
+#intento con positive lookbehind para obtener sólo el nombre del autor sin "Author:" 
+# authors <- str_subset(lines, "(?<=^Author:\\s)\\w+,\\s\\w+\\s\\w+\\.*\\n")
