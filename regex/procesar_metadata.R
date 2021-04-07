515e6fbe63083ad7f6a48058c562a57de3da188c
Silvia Eunice Gutiérrez de la Torre <segutierrez@colmex.mx>
2021-04-06 22:33:41 -0500

    :scroll: notas añadidas

--- regex/procesar_metadata.R
+++ regex/procesar_metadata.R
@@ -12,6 +12,9 @@ authors <- str_subset(lines, "(^Author:.\\w+\\,\\s\\w+)")
 #View(titles)
 #Nota: sólo regresa un string vacío antes del primer patrón detectado
 #Ver: https://twitter.com/espejolento/status/1379633590481260548
+#Nota 2: se requiere hacer slice(lag(str_detect..) para trabajar con los índices
+#pero para esto tiene que ser un dataframe
+#Ver: https://twitter.com/LuisDVerde/status/1379635446288949248
 
 
 #intento para extraer títulos:
