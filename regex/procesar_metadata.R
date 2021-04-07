f3dc3c72f8bdc596a3e23decae66567e4ee05c30
Silvia Eunice Gutiérrez de la Torre <segutierrez@colmex.mx>
2021-04-07 12:40:08 -0500

    :gift: código con las enseñanzas de @rivaquiroga @svrvhi

--- regex/procesar_metadata.R
+++ regex/procesar_metadata.R
@@ -1,43 +1,34 @@
-library(tidyverse)
-library(stringr)
-
-text <- file("metadata_german_departments.txt", open = "r")
-lines <- readLines(text)
-View(lines)
-
-authors <- str_subset(lines, "(^Author:.\\w+\\,\\s\\w+)")
-
-#intento con lag:
-#titles <- lag(str_subset(lines, "(^Author:.\\w+\\,\\s\\w+)"))
-#View(titles)
-#Nota: sólo regresa un string vacío antes del primer patrón detectado
-#Ver: https://twitter.com/espejolento/status/1379633590481260548
-#Nota 2: se requiere hacer slice(lag(str_detect..) para trabajar con los índices
-#pero para esto tiene que ser un dataframe
-#Ver: https://twitter.com/LuisDVerde/status/1379635446288949248
+#Propuesta de @rivaquiroga (https://github.com/silviaegt/dudas/issues/2)
+#Modificada gracias a los resultados de @svrvhi (https://github.com/silviaegt/dudas/blob/master/regex/propuesta_sarahi-aguilar.R)
+#Adaptación y notas @espejolento
 
 
-
+# Cargar paquetes ---------------------------------------------------------
 library(readr)
 library(dplyr)
 library(stringr)
 library(tidyr)
 
-metadata <- read_tsv("https://raw.githubusercontent.com/silviaegt/dudas/master/regex/metadata_german_departments", col_names = FALSE)
+# Leer archivo ------------------------------------------------------------
+text <- file("metadata_german_departments.txt", open = "r")
+lines <- readLines(text, encoding = "UTF-8") #conviene usar este método para que no se salte las files que empiezan con caracteres especiales
+metadata <- as_tibble(lines)
 
-metadata %>%
-  mutate(X1 = case_when(
-    lead(str_detect(X1, "^Author:")) ~ str_c("Title: ", X1),
-    TRUE ~ as.character(X1)
+
+# Transformar de filas a columnas usando expresiones regulares y pivot_wider------------
+metadata <- metadata %>%
+  mutate(value = case_when(
+    lead(str_detect(value, "^Author:")) ~ str_c("Title: ", value),
+    #Si fila X tiene debajo algo que coincide con str_detect(X1, "^Author:"), le agrega "Title: " a esa fila X.
+    TRUE ~ as.character(value)
   )) %>% 
-  filter(str_detect(X1, "^Author: |^Title: ")) %>% 
-  separate(X1, into = c("variable", "data"), extra = "merge") %>% 
-  mutate(id = rep(1:83, each = 2), .before = variable) %>% 
-  pivot_wider(names_from = variable, values_from = data) 
+  filter(str_detect(value, "^Author: |^Title: |^Publication info: |^Subject: ")) %>% #acá se filtran las líneas con el patrón deseado
+  separate(value, into = c("variable", "data"), sep = ": ", extra = "merge") %>% #se dividen las variables del contenido usando ": " como separador
+  mutate(id = rep(1:100, each = 4), .before = variable) %>% # se le da el mismo id a los grupos de metadatos
+  pivot_wider(names_from = variable, values_from = data) %>% #usar variable como nombre de columna y rellenar con el contenido (data)
+  rename(Publication_info = 'Publication info') %>% #renombrar columna para evitar nombres con espacios
+  select(., -id) #eliminar columna de "ids"
 
 
-#intento para extraer títulos:
-#titles <- str_subset(lines, "(\\n).*(\\nAuthor:\\s)")
-
-#intento con positive lookbehind para obtener sólo el nombre del autor sin "Author:" 
-# authors <- str_subset(lines, "(?<=^Author:\\s)\\w+,\\s\\w+\\s\\w+\\.*\\n")
+# Crear csv con resultado -------------------------------------------------
+write_csv(metadata, "metadata.csv")
--- regex/propuesta_riva-quiroga.R
+++ regex/propuesta_riva-quiroga.R
@@ -6,16 +6,16 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 
-metadata <- read_tsv("https://raw.githubusercontent.com/silviaegt/dudas/master/regex/metadata_german_departments", col_names = FALSE)
-?lead
-auti <- metadata %>%
-  mutate(X1 = case_when(
-    lead(str_detect(X1, "^Author:")) ~ str_c("Title: ", X1), #aquí se identifica el tíutulo y se le añade Title:
-    TRUE ~ as.character(X1)
-  )) %>% 
-  filter(str_detect(X1, "^Author: |^Title: ")) %>% #acá se filtran las líneas con Author y Title
-  separate(X1, into = c("variable", "data"), extra = "merge") %>% #se divide "Title:" y "Author:" (en variable) del contenido (en data)
-  mutate(id = rep(1:83, each = 2), .before = variable) %>%  # se le da el mismo id a los pares de Title/Author
-  pivot_wider(names_from = variable, values_from = data) 
+text <- file("metadata_german_department.txt", open = "r")
+lines <- readLines(text, encoding = "UTF-8")
+metadata <- as_tibble(lines)
 
-#se obtienes 83 observaciones
+metadata %>%
+  mutate(value = case_when(
+    lead(str_detect(value, "^Author:")) ~ str_c("Title: ", value),
+    TRUE ~ as.character(value)
+  )) %>% 
+  filter(str_detect(value, "^Author: |^Title: ")) %>% 
+  separate(value, into = c("variable", "data"), extra = "merge") %>% 
+  mutate(id = rep(1:100, each = 2), .before = variable) %>% 
+  pivot_wider(names_from = variable, values_from = data)
